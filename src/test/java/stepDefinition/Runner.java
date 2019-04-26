738737233660b4e0d88cf9601d6e08361bd4e403
sanzida01 <sanzidaqa01@gmail.com>
2019-04-25 23:37:19 -0400

    test

--- src/test/java/stepDefinition/Runner.java
+++ src/test/java/stepDefinition/Runner.java
@@ -13,12 +13,12 @@ import cucumber.api.junit.Cucumber;
 		plugin = {"pretty", "html:target/cucumber-html-report","json:target/cucumber.json"},
 		monochrome = true, //Display the console output in a readable format
 		dryRun = false,
-		//tags = {"@SMOKE"}
+		tags = {"@SMOKE"}
 		//tags = {"@Regression"}
 		//tags = {"~@Test4"}   // will run all Scenarios without the Test4 tag
 		//tags = {"@SmoketestFiP2"}
 		//tags = {"@SmoketestFiP7,@SmoketestSP2"}
-		tags = {"@Regression"}
+		//tags = {"@Regression"}
 		)
 public class Runner{
 	
